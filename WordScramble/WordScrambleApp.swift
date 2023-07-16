48c53f766972b4fcc9c24b7ea07a2009a30fca6c
Caden Huffman <cadenhuf@bu.edu>
2023-07-16 14:46:58 -0400

    Just uploading to repository for later

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Caden Huffman on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
